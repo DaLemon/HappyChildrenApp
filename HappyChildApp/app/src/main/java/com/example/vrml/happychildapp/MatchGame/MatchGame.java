0f5048d47b56d23c8f9e8f1ae4f6fb701115c50f
Nora <vivian2934com@gmail.com>
2017-10-29 03:48:10 +0800

    MatchGame download image from Firebase+拿取左側點的位置

--- HappyChildApp/app/src/main/java/com/example/vrml/happychildapp/MatchGame/MatchGame.java
+++ HappyChildApp/app/src/main/java/com/example/vrml/happychildapp/MatchGame/MatchGame.java
@@ -1,11 +1,14 @@
 package com.example.vrml.happychildapp.MatchGame;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -15,13 +18,18 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.vrml.happychildapp.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,6 +44,8 @@ public class MatchGame extends AppCompatActivity {
     private MotionEvent moveEvent;
     private int range = 0;//點的有效範圍
     MatchGameData matchGameData ;
+
+    final long ONE_MEGABYTE = 1024*1024;
 
 
 
@@ -83,21 +93,58 @@ public class MatchGame extends AppCompatActivity {
         }
 
     }
+    int ImageIndex = 0;
     public void setData(String[] Chinese,String[] Phonetic,String[] ImagePath){
+        StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+        final long ONE_MEGABYTE = 128*128;
 
         for (int i = 0;i<5;i++){
 //            StorageReference storageReference;
             this.Chinese[i].setText(Chinese[i]);
             this.Phonetic[i].setText(Phonetic[i]);
+            StorageReference temp = storageRef.child(ImagePath[i]);
+            temp.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                @Override
+                public void onSuccess(byte[] bytes) {
+                    Bitmap tmep = BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+                    MatchGame.this.imageViews[ImageIndex++].setImageBitmap(tmep);
+                }
+            });
 //            this.imageViews[i].setImageBitmap();
+
         }
     }
+    public void Download(ImageView imageView){
+        StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+        StorageReference temp = storageRef.child(imageView.getTag().toString());
+        temp.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                Bitmap tmep = BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+                MatchGame.this.imageViews[ImageIndex++].setImageBitmap(tmep);
+                Log.e("DEBUG",""+ImageIndex+"   "+MatchGame.this.imageViews[ImageIndex-1].getTag());
+                if (ImageIndex<5) {
+                    Download(imageViews[ImageIndex]);
+                }
+            }
+
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e("DEBUG",e.toString());
+            }
+        });
+    }
     public void setData(MatchGameData matchGameData){
+
         for (int i = 0;i<5;i++){
             this.Chinese[i].setText(matchGameData.getChineseData()[i]);
             this.Phonetic[i].setText(matchGameData.getPhoneticData()[i]);
-//            this.imageViews[i].setImageBitmap();
+            String path = matchGameData.getImagePathData()[i];
+
+            imageViews[i].setTag(path);
         }
+        Download(imageViews[ImageIndex]);
     }
     private void getdataFromFirebase() {
 
@@ -175,12 +222,14 @@ public class MatchGame extends AppCompatActivity {
             for (int i = 0; i < 5; i++, startY += height / 5) {
                 dot[i] = new Point(startX, startY);
                 dot[i + 5] = new Point(startX + width * 25 / 28, startY);//慢慢兜出來的＝～＝
-                Log.e("DEBUG","X:"+dot[i].x+"  Y:"+dot[i].y);
+                //Log.e("DEBUG","X:"+dot[i].x+"  Y:"+dot[i].y);
             }
         }
 
         private boolean isTouch = false;//判斷是不是在dot的範圍內
         int down = 0;
+        int[] phoneticDot={0,0,0,0,0},chineseDot={0,0,0,0,0},imageDot={0,0,0,0,0};
+
         @Override
         public boolean onTouchEvent(MotionEvent event) {
             if (isWrong) {
@@ -197,6 +246,9 @@ public class MatchGame extends AppCompatActivity {
                         p1X = dot[checkpoint].x;
                         p1Y = dot[checkpoint].y;
 
+                        getdot(checkpoint);
+                        for (int i=0;i<5;i++)
+                            Log.e("DEBUG","down"+phoneticDot[i]+"  "+imageDot[i]);
                         invalidate();
 
                     }
@@ -218,6 +270,9 @@ public class MatchGame extends AppCompatActivity {
                         mPoint.add(p2X);
                         mPoint.add(p2Y);
 
+                        getdot(checkpoint);
+                        for (int i=0;i<5;i++)
+                            Log.e("DEBUG","up"+phoneticDot[i]+"  "+imageDot[i]);
                         invalidate();
                     }
 
@@ -230,6 +285,7 @@ public class MatchGame extends AppCompatActivity {
 
                         p2X = event.getX();
                         p2Y = event.getY();
+
                         invalidate();
                     }
                     break;
@@ -254,6 +310,16 @@ public class MatchGame extends AppCompatActivity {
             }
 
             return -1;
+        }
+
+        public void getdot(int dot) {
+
+            if (dot < 5) {
+                phoneticDot[dot] = dot;
+
+            } else {
+                imageDot[dot%5] = dot%5;
+            }
         }
     }
 
